97ebfea2e0c868286d7099ce14a33596a0b14cc4
CrazyTapok <main.profile.mail@gmail.com>
2023-05-26 21:03:32 +0300

    Fill the Unit project with tests (#4)

---
+++ tests/Validator/InitDataValidatorTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace TgWebValid\Test\Validator;
+
+use PHPUnit\Framework\TestCase;
+use TgWebValid\Entities\InitData;
+use TgWebValid\Validator\InitDataValidator;
+
+class InitDataValidatorTest extends TestCase
+{
+    protected InitDataValidator $validator;
+
+    protected string $initData = 'query_id=AAE5gYJAAAAAADmBgkD7IagW&user=%7B%22id%22%3A1082294585%2C%22first_name%22%3A%22%D0%A1%D0%B5%D1%80%D0%B3%D1%96%D0%B9%22%2C%22last_name%22%3A%22%D0%97%D0%B0%D1%81%D0%B0%D0%B4%D0%B8%D0%BD%D1%81%D1%8C%D0%BA%D0%B8%D0%B9%22%2C%22username%22%3A%22CrazyTapokUA%22%2C%22language_code%22%3A%22uk%22%7D&auth_date=1684086610&hash=f0f336451c74fc794e2b0b9fcaf3e27e16ca74afbfd0958a8d21efd9e8e2b53c';
+
+    protected function setUp(): void
+    {
+        $this->validator = new class ('5854973744:AAFnq4HoybEzqCJ-8HYHY_zlvkc_-H-kXq4') extends InitDataValidator {};
+    }
+
+    public function testParse(): void
+    {
+        $parsed = $this->validator->parse($this->initData);
+
+        $this->assertEquals([
+            'query_id' => 'AAE5gYJAAAAAADmBgkD7IagW',
+            'user' => '{"id":1082294585,"first_name":"Сергій","last_name":"Засадинський","username":"CrazyTapokUA","language_code":"uk"}',
+            'auth_date' => 1684086610,
+            'hash' => 'f0f336451c74fc794e2b0b9fcaf3e27e16ca74afbfd0958a8d21efd9e8e2b53c'
+        ], $parsed);
+    }
+
+    public function testValidated(): void
+    {
+        $result = $this->validator->validate($this->initData);
+
+        $this->assertInstanceOf(InitData::class, $result);
+    }
+
+    public function testFail(): void
+    {
+        $result = $this->validator->validate($this->initData . '1');
+
+        $this->assertFalse($result);
+    }
+}
---
+++ tests/Validator/LoginWidgetValidatorTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace TgWebValid\Test\Validator;
+
+use PHPUnit\Framework\TestCase;
+use TgWebValid\Entities\LoginWidget;
+use TgWebValid\Validator\LoginWidgetValidator;
+
+class LoginWidgetValidatorTest extends TestCase
+{
+    protected LoginWidgetValidator $validator;
+
+    protected function setUp(): void
+    {
+        $this->validator = new class ('5854973744:AAFnq4HoybEzqCJ-8HYHY_zlvkc_-H-kXq4') extends LoginWidgetValidator {};
+    }
+
+    public function testValidated(): void
+    {
+        $result = $this->validator->validate([
+            'auth_date' => 1685117908,
+            'first_name' => 'Сергій',
+            'hash' => 'e286fe20edabc0f086ba11bad5eead92a67776d01ac97e814ddfb683974d16e9',
+            'id' => 1082294585,
+            'last_name' => 'Засадинський',
+            'photo_url' => 'https://t.me/i/userpic/320/7gMg9ZfoSzMQcLwYiEj4rLAofXXn0wOBV9HXGb6c1T0.jpg',
+            'username' => 'CrazyTapokUA'
+        ]);
+
+        $this->assertInstanceOf(LoginWidget::class, $result);
+    }
+
+    public function testFail(): void
+    {
+        $result = $this->validator->validate([
+            'auth_date' => 1679130118,
+            'first_name' => 'Сергій',
+            'hash' => 'e286fe20edabc0f086ba11bad5eead92a67776d01ac97e814ddfb683974d16e9',
+        ]);
+
+        $this->assertFalse($result);
+    }
+}
